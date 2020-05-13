44eaf161125acc6491f067e3c6422b65a34b7123
annie520143 <annie520143@gmail.com>
2020-05-13 16:57:24 +0800

    fix little bug

--- method_dl/model.py
+++ method_dl/model.py
@@ -28,7 +28,7 @@ def simpleDNN(feature_dim, units, atv, loss):
 
 def simpleRNN(train_packets, atv, loss):
     model = Sequential()
-    model.add(LSTM(100, return_sequence = True, input_shape = train_packets[0].shape))
+    model.add(LSTM(100, return_sequences = True, input_shape = train_packets.shape))
     model.add(LSTM(100))
     model.add(Dense(8))
     model.add(Dense(units=2, kernel_initializer='uniform', activation=atv))
